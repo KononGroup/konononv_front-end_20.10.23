640ce811945c15e6afe5e56705a25c5db65d7a36
KononGroup <kononovkoko@gmail.com>
2023-10-24 13:20:22 +0300

    created 2, 3, 4 ,5, 6 tasks

---
+++ index.js
@@ -0,0 +1,64 @@
+// //1.
+
+// const userName = prompt('What is your name', 'John');
+
+// userName;
+
+// alert(`Hello,${userName}! How are you ?`);
+
+////////////////////////////////////////////////////////////////////////////////////////
+
+// //2.
+
+// const question1 = prompt('Write something');
+// const question2 = prompt('Write something');
+// const question3 = prompt('Write something');
+
+// const requestQuestions = console.log(`${question1},${question2},${question3}`);
+
+// const number = prompt('Write five digit number', '12345');
+
+// const requestNumber = console.log(`${number[0]} ${number[1]} ${number[2]} ${number[3]} ${number[4]}`);
+
+
+////////////////////////////////////////////////////////////////////////////////////////
+
+// //3.
+
+// const firstOperand =+ prompt('addition, subtraction, multiplication and division of two numbers', 'Write first number');
+// const secondOperand =+ prompt('addition, subtraction, multiplication and division of two numbers', 'Write second number');
+
+
+
+// alert(`${firstOperand} + ${secondOperand} = ${firstOperand + secondOperand}
+//     ${firstOperand} - ${secondOperand} = ${firstOperand - secondOperand}
+//     ${firstOperand} * ${secondOperand} = ${firstOperand * secondOperand}
+//     ${firstOperand} / ${secondOperand} = ${firstOperand / secondOperand}
+// `);
+
+////////////////////////////////////////////////////////////////////////////////////////
+
+// //4.
+
+// const promptCalculator = prompt('what do you want to do ?add, subtract, multiply or divide', '+, -, *, /');
+
+// const firstNumber =+ prompt('Write first number', 'Write first number');
+// const secondNumber =+ prompt('Write second number', 'Write second number');
+
+// alert(`${firstNumber} + ${secondNumber} = ${firstNumber + secondNumber}
+//     ${firstNumber} - ${secondNumber} = ${firstNumber - secondNumber}
+//     ${firstNumber} * ${secondNumber} = ${firstNumber * secondNumber}
+//     ${firstNumber} / ${secondNumber} = ${firstNumber / secondNumber}
+// `);
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//5.
+
+// const quantityHours =+ prompt('write the number of hours', '10');
+
+// const howManySeconds = `${quantityHours * 3600}`;
+
+// alert(`${howManySeconds}`);
+
+
