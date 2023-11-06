d846a80ec7d9e938d233e5cb54ee9c340c2013af
KononGroup <kononovkoko@gmail.com>
2023-11-06 18:58:40 +0200

    homework4

--- index.js
+++ index.js
@@ -1,138 +1,116 @@
-//task 10 writing loops
-//1
-// for (let i = 10; i < 21; i++){
-//     console.log(`(${i}`);
-// };
+//12 Array search
 
-//2
-// for (let i = 10; i < 21; i++){
-//     console.log(`${i**2}`);
-// };
+const array = [16, -37, 54, -4, 72, -56, 47, 4, -16, 25, -37, 46, 4, -51, 27, -63, 4, -54, 76, -4, 12, -35, 4, 47];
 
-//3
-// for (let i = 0; i < 11; i++){
-//     console.log(`${7 * i}`);
-// };
-
-//4
-// let i = 0;
-// let sum = 0;
-// for (  i ; i < 16; i++) {
-//     if (i % 2 === 0) {
-//         sum += i ;
-//     }
-// };
-// console.log(sum);
-
-//5
-// let i = 15;
-// let sum =1;
-// for (  i ; i < 36; i++) {
-//     if (i % 2 === 0) {
-//         sum *= i;
-//     }
-// };
-// console.log(sum);
-
-//6
-// let sum = 0;
-// for (let i = 0; i < 501; i++) {
-//     if (i % 2 === 0) {
-//         sum += i;
-//         console.log(sum);
-//     }
-// };
-// const getAverage = sum / 500;
-
-// console.log(getAverage);
-
-//7
-// let i = 30;
-// let sum = 0;
-// for (  i ; i < 81; i++) {
-//     if (i % 2 === 0) {
-//         sum += i;
-//     }
-// };
-// console.log(sum);
-
-//8
-// let i = 100;
-// for (  i ; i < 202; i++) {
-//     if (i % 3 === 0) {
-//         console.log(i);
-//     }
-// };
-
-//9,10, 11
-// let naturalNum = +prompt('Enter any natural number', '...');
+//1.Find the sum and quantity of positive elements.
 // let count = 0;
+// let howManyPositiveNumbers = 0;
+// for (const item of array) {
+//     if (item > 0) {
+//         count += item;
+//         howManyPositiveNumbers++;
+//     }
+// }
+// console.log(count, howManyPositiveNumbers);
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+//2.Find the minimum array element and its sequence number.
+// let min = array[0];
+// for (const item of array) {
+//     if (item < min) {
+//         min = item;
+//     };
+// };
+// console.log(min, array.indexOf(min));
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+// 3.Find the maximum array element and its sequence number.
+//  let max = array[0];
+//   for (const item of array) {
+//     if (item > max) {
+//       max = item;
+//     }
+// };
+// console.log(max, array.indexOf(max));
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+//4. Determine the quantity of negative elements.
+// let howManyNegativeNumbers = 0;
+// for (const item of array) {
+//     if (item < 0) {
+//         howManyNegativeNumbers++;
+//     }
+// }
+// console.log(howManyNegativeNumbers);
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+//5.Find the quantity of odd positive elements.
+// let count = 0;
+// for (const item of array) {
+//     if (item > 0 && item % 2 === 1) {
+//         count++;
+//    }
+// };
+// console.log(count);
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+//6. Find the quantity of even positive elements
+// let count = 0;
+// for (const item of array) {
+//     if (item > 0 && item % 2 === 0) {
+//         count++;
+//    }
+// };
+// console.log(count);
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+//7.Find the sum of even positive elements.
 // let sum = 0;
-// for (let i = 2; i * 2 <= naturalNum; i++) {
-//   if (naturalNum % i === 0) {
-//       console.log(`divider ${naturalNum} is ${i}`);
-
-//       if (i % 2 === 0) {
-//           count++
-//           sum += i;
-//       }
-//   }
+// for (const item of array) {
+//     if (item > 0 && item % 2 === 0) {
+//         sum += item;
+//    }
 // };
+// console.log(sum);
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-// console.log(`number of even divisors: ${count}
-// sum of even divisors: ${sum}`);
 
-//12
-// let table = "";
-
-// for (a = 1; a <= 10; a++){
-
-//     for (b = 1; b <= 10; b++) {
-
-//         const result = String(a * b);
-//         tt += ' '.repeat(4 - result.length) + result;
-//     }
-//     tt += '\n';
-// }
-// console.log(table);
-//////////////////////////////////////////////////////////////////////////////////////////////////
-
-//task 11 Folding cycles
-
-//1
-// for (let i = 20; i < 31; i += 0.5){
-//     console.log(i);
-// }
-
-//2
-
-// const dollarInUah = 27;
-// for (let i = 10; i < 101; i += 10 ) {
-//     let result = dollarInUah * i;
-//     console.log(result);
-// }
-
-//3, 4, 5 
-// let evenNum = +prompt('Enter any even number', '...');
-// let N = evenNum;
-// for (let i = 0; i < 100; i++) {
-//     if (N > Math.pow(i, 2)) {
-//       console.log(i);
-//     }
+//8.Find the sum of odd positive elements.
+// let sum = 0;
+// for (const item of array) {
+//     if (item > 0 && item % 2 === 1) {
+//         sum += item;
+//    }
 // };
-// for (let i = 2; i * 2 <= N; i++) {
-//   if (N % i === 0) {
-//       console.log(`you number is not prime`);
-//         break
-//   } else {
-//       console.log(`you number is prime`);
-//       break
-//   }
-// };
-// console.log(N);
+// console.log(sum);
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-// while (N % 3 === 0) {
-//     N /= 3;
-// };
+//9.Find the product of positive elements.
 
-// console.log(N === 1 ? true : false );
+// let productOfFactors = 1;
+// for (const item of array) {
+//     if (item > 0) {
+//         console.log(item);
+//         productOfFactors *= item;
+//    }
+// };
+// console.log(productOfFactors);
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//10.Find the largest among the elements of the array, set the others to zero.
+
+ let max = array[0];
+  for (const item of array) {
+    if (item > max) {
+      max = item;
+    }
+};
+
+array.splice(0, array.length, max);
+console.log(array);
