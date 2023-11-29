33e06cbe8480c261a546b9bd44eec7ac33ca3db1
KononGroup <kononovkoko@gmail.com>
2023-11-29 20:13:12 +0200

    hm9

--- HW17task3.js
+++
@@ -1,8 +0,0 @@
-// const userElements = prompt('enter array values');
-// const userElements2 = prompt('enter nested array values');
-// const twoDimensionalArray = [];
-// const userElementsArr = userElements.split(" ").filter((el)=> el !== "");
-// const userElements2Arr = userElements2.split(" ").filter((el)=> el !== "");
-// twoDimensionalArray.push(userElementsArr)
-// twoDimensionalArray.push(userElements2Arr)
-// console.log(twoDimensionalArray);
---
+++ HW18task1.js
@@ -0,0 +1,12 @@
+
+// const raiseToAPower = (num, degree) => {
+//       if (degree == 1) {
+//     return num;
+//   } else {
+//     return num * raiseToAPower(num, degree - 1);
+//   }
+
+   
+// }
+
+// console.log(raiseToAPower(2,5));
